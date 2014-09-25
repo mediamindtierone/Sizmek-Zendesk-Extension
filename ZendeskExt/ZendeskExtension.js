b131d2b25fa41508e249bd9b459a7381d67099e6
mediamindtierone <mediamindtierone@gmail.com>
2014-09-25 23:27:15 +0800

    Revert "recoded zendeskExtension"
    This reverts commit f304543c7d4dc34b18343942df76254763ce92e2.

--- ZendeskExt/ZendeskExtension.js
+++ ZendeskExt/ZendeskExtension.js
@@ -1,17 +1,13 @@
 var SZE_interval = setInterval(function() {
 	var SZE;
 	SZE = chrome.runtime;
-	var sz_rnd = Math.random() * 1000000;
 	if(typeof(SZE)!="undefined") {
 		clearInterval(SZE_interval);
-		
 		var szemod = document.createElement("script");
-		szemod.src = "https://secure-ds.serving-sys.com/burstingres/CustomScripts/ZendeskExtension_plugin.js?rnd=" + sz_rnd;
-		
-		var timeago = document.createElement("script");
-		timeago.src = chrome.extension.getURL("scripts/moment.min.js")+"?rnd=" + sz_rnd;
-		
+		szemod.src="https://secure-ds.serving-sys.com/burstingres/CustomScripts/ZendeskExtension_plugin.js?rnd="+(Math.random()*1000000);
 		document.head.appendChild(szemod);
+		var timeago = document.createElement("script");
+		timeago.src=chrome.extension.getURL("scripts/moment.min.js")+"?rnd="+(Math.random()*1000000);
 		document.head.appendChild(timeago);
 	}
 }, 1000); 
