f304543c7d4dc34b18343942df76254763ce92e2
mediamindtierone <mediamindtierone@gmail.com>
2014-09-25 23:18:14 +0800

    recoded zendeskExtension
    Recoded ZendeskExtension.

--- ZendeskExt/ZendeskExtension.js
+++ ZendeskExt/ZendeskExtension.js
@@ -1,13 +1,17 @@
 var SZE_interval = setInterval(function() {
 	var SZE;
 	SZE = chrome.runtime;
+	var sz_rnd = Math.random() * 1000000;
 	if(typeof(SZE)!="undefined") {
 		clearInterval(SZE_interval);
+		
 		var szemod = document.createElement("script");
-		szemod.src="https://secure-ds.serving-sys.com/burstingres/CustomScripts/ZendeskExtension_plugin.js?rnd="+(Math.random()*1000000);
-		document.head.appendChild(szemod);
+		szemod.src = "https://secure-ds.serving-sys.com/burstingres/CustomScripts/ZendeskExtension_plugin.js?rnd=" + sz_rnd;
+		
 		var timeago = document.createElement("script");
-		timeago.src=chrome.extension.getURL("scripts/moment.min.js")+"?rnd="+(Math.random()*1000000);
+		timeago.src = chrome.extension.getURL("scripts/moment.min.js")+"?rnd=" + sz_rnd;
+		
+		document.head.appendChild(szemod);
 		document.head.appendChild(timeago);
 	}
 }, 1000); 
